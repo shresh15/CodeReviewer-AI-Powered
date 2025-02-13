316bfe1af8ad50a4014e250b24258365a3e28310
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-13 12:31:11 +0530

    Ai routes written here

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 const express = require("express");
+
+const aiRoutes = require("./routes/ai.routes");
 const app = express();
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/ai", aiRoutes);
 module.exports = app;
