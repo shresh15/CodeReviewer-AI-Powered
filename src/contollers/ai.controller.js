852147a5f3969236a06f6a95fc2c8474af711988
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-13 12:22:06 +0530

    Controllers

---
+++ src/contollers/ai.controller.js
@@ -0,0 +1,10 @@
+const aiService = require("../services/ai.service");
+
+module.exports.getResponse = async (req, res) => {
+  const prompt = req.query.prompt;
+  if (!prompt) {
+    return res.status(400).send("prompt is required");
+  }
+  const response = await aiService(prompt);
+  res.send(response);
+};
