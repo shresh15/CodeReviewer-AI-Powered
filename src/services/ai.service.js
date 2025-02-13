c4afdfe070172ecfd81d51e72a7fd2b9bd9dd756
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-13 12:23:49 +0530

    AI services integrated

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -5,5 +5,11 @@ const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 const prompt = "Explain how AI works";
 
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+const result = model.generateContent(prompt);
+//console.log(result.response.text());
+
+async function generateContent(prompt) {
+  const result = await model.generateContent(prompt);
+  return result.response.text();
+}
+module.exports = generateContent;
