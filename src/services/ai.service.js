a570529df07f3941d185c6471bf5bb9411ceba48
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-14 09:44:40 +0530

    Instructions given to AI

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -1,12 +1,12 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-const prompt = "Explain how AI works";
-
-const result = model.generateContent(prompt);
-//console.log(result.response.text());
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+  systemInstruction: `you are a code reviewer who has an expertise in development. You look for the code and find problems and suggest the solution to the developer.
+  
+  You always try to find the best solution for the developer and also try to make the code more efficient and clean`,
+});
 
 async function generateContent(prompt) {
   const result = await model.generateContent(prompt);
