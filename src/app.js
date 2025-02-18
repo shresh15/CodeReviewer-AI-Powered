33623766501839dfb4b78324537bbc4bf63dca8a
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-18 23:11:43 +0530

    js changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const aiRoutes = require("./routes/ai.routes");
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 app.use(express.json()); // ✅ Parse JSON requests
 
 app.get("/", (req, res) => {
